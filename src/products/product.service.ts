34976feb5561e609631f519fa219ab5303a0bcf2
event <event@z2t13c3.42bangkok.com>
2026-03-25 11:12:16 +0700

    P01

---
+++ src/products/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from './product.interface';
+import { ApiResponse } from '../interfaces/response.interface';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const filepath = join(__dirname, '../../data/products.json');
+
+@Injectable()
+export class ProductService {
+  findAll(): ApiResponse<Product[]> {
+    const readProductData = readFileSync(filepath, 'utf-8');
+    const productData = JSON.parse(readProductData) as Product[];
+    
+    return {
+      success: true,
+      data: productData,
+      message: 'Fetched products successfully',
+    };
+  }
+}
